b91334b65e64600b57b91265585e00419a7b4639
Malcolm Jones <bossjones@theblacktonystark.com>
2019-05-21 22:07:58 -0400

    wip: fix pyenv init

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,3 +2,7 @@
 echo "================================================="
 echo "pyenv versions: $(pyenv versions)"
 echo "================================================="
+
+echo 'export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
